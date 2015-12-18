5cafeed05e5414d58d6c889b0677524711bd4563
Cosmos Darwin <cosmosdarwin@live.com>
2015-12-18 00:14:43 -0800

    html content

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer( function(req, res) {
-	res.writeHead(200);
-	res.end('Hello, world.');
+	res.writeHead(200, {"Content-Type" : "text/html"});
+	res.end('<blink>Hello, world.</blink>');
 });
 
 server.listen(8080);
