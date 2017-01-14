07341652e18f0a3520bcdca729f9921509848cca
Cosmos Darwin <cosmosd@microsoft.com>
2017-01-13 23:55:14 -0800

    gut entirely

--- server.js
+++ server.js
@@ -1,38 +1,10 @@
-// Express Framework
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-// Jade Templating Engine
-var jade = require('jade');
-app.set('view engine', 'jade');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// MIDDLEWARE
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
-app.use(express.static('public'));
-
-// ROUTES
-
-app.get('/', function(req, res) {
-  res.redirect('/play');
-});
-
-app.get('/play', function(req, res) {
-    res.render('play');
-});
-
-app.get('/billboard', function(req, res) {
-    res.render('billboard');
-});
-
-// 404
-
-app.use(function(req, res, next) {
-	res.setHeader('Content-Type', 'text/html');
-	res.send(404, 'You are lost.');
-});
-
-// RUN
-
-app.listen(3000);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
